df5130568a99bd395e9fb1c052834eeeaa793a4b
AMK9978 <amk9978@gmail.com>
2020-11-02 11:38:50 +0330

    initial commit

---
+++ nutrition/models.py
@@ -0,0 +1,26 @@
+# Create your models here.
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(),
+                          editable=False)
+    charge = models.IntegerField
+    is_chef = models.BooleanField(default=False)
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=48)
+    price = models.IntegerField(default=0)
+    capacity = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date published')
+
+
+class FoodReserve(models.Model):
+    date = models.DateTimeField
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_id = models.ForeignKey(Food, on_delete=models.CASCADE)
